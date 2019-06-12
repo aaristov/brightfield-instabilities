dcd5da20fb2e4cb887ff15bf615d24e38b888da2
aaristov <aaristov@pasteur.fr>
2019-06-12 17:18:55 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from brightfield_instabilities.version import __version__
+
+setup(name='brightfield_instabilities',
+      version=__version__,
+      description='Highlights local differences in brightfield tiff stack',
+      author='Andrey Aristov',
+      author_email='aaristov@pasteur.fr',
+      url='https://github.com/aaristov/brightfield-instabilities',
+      packages=['brightfield_instabilities'],
+      requires=['numpy', 'scipy', 'imageio', 'tqdm']
+     )
