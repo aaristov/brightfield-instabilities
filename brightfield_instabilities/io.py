c93053660d1d8033ec06839ace86dcb9d0305238
aaristov <aaristov@pasteur.fr>
2019-06-21 15:11:53 +0200

    add last file, create subfolder

--- brightfield_instabilities/io.py
+++ brightfield_instabilities/io.py
@@ -29,12 +29,15 @@ def select_two_images(fileList:list):
     return sel
 
 def select_multi_images(fileList:list, skip:int=-1):
-    sel = [fileList[0]]
+    sel = []
     if skip <= 0:
+        sel.append(fileList[0])
         sel.append(fileList[-1])
     else:
         for i in fileList[::skip]:
             sel.append(i)
+        if i != fileList[-1]:
+            sel.append(fileList[-1])
     print('Selected ', list(os.path.basename(p) for p in sel))
     return sel
 
@@ -44,4 +47,27 @@ def read_images(*paths, handler=open_tiff):
     '''
     print('Reading ', list(os.path.basename(p) for p in paths))
     for p in paths:
-        yield handler(p)
+        img = handler(p)
+        yield img
+
+def create_subfolder(current_path, folder_name):
+    '''
+    Creates a folder inside current folder.
+    Returns 
+        the new subfolder path
+    Raises
+        IOError, AssertionError if folder has not be created
+    '''
+    assert os.path.isdir(current_path)
+    desired_path = os.path.join([current_path, folder_name])
+    if not os.path.isdir(desired_path):
+        try:
+            os.mkdir(desired_path)
+            assert os.path.isdir(desired_path), 'Unable to create a folder.'
+            print(f'Create {folder_name} subfolder')
+            return desired_path
+        except IOError as e:
+            print(f'IOError, {e.args}')
+            raise e
+    
+
