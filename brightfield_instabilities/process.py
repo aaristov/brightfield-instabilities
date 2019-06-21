dba3093c29e57ced9c61949073e7a55c44aaa99d
aaristov <aaristov@pasteur.fr>
2019-06-21 15:13:08 +0200

    processing correlations in series

--- brightfield_instabilities/process.py
+++ brightfield_instabilities/process.py
@@ -14,6 +14,7 @@ def process_folder(
     smooth, 
     max_shift,
     cc_skip,
+    skip_frames=-1,
     filename=OUT_FILENAME
     ):
     
@@ -25,33 +26,45 @@ def process_folder(
     assert smooth >= 0
 
     fList = io.scan_folder(folderPath)
-    two_files = io.select_two_images(fList)
-    im0, im1 = io.read_images(*two_files)
+    selected_paths = io.select_multi_images(fList, skip=skip_frames)
     
-    out = []
-    try:
-        out = cc.sliding_corr(
-            im0, 
-            im1, 
-            size=size, 
-            smooth=smooth,
-            cc_skip=cc_skip,
-            max_shift=max_shift)
+    assert len(selected_paths) > 1, 'File sequence too short'
 
-    except KeyboardInterrupt:
-        print('User stop, exiting')
-        return False
+    im0 = io.open_tiff(selected_paths[0])
     
-    finally:    
-        parentDir = os.path.dirname(folderPath)
-        channels = ['corr', 'x', 'y']
-        if len(out):
-            for i, channel in enumerate(out):
-                fName = '_'.join([os.path.basename(folderPath), channels[i], filename])
-                savePath = os.path.join(parentDir, fName)
-                io.save_tiff(channel, savePath)
-        # exit(0)
-        else:
-            print('Result is empty.')
+    print(im0.shape)
+
+    for i, f in enumerate(selected_paths[1:]):
+        f_short = os.path.basename(f)
+        im1 = io.open_tiff(f)
+        print(f'Processing {f_short}')
+    
+        out = []
+        try:
+            out = cc.sliding_corr(
+                im0, 
+                im1, 
+                size=size, 
+                smooth=smooth,
+                cc_skip=cc_skip,
+                max_shift=max_shift)
+
+        except KeyboardInterrupt:
+            print('User stop, exiting')
+            return False
+    
+        finally:    
+            parentDir = os.path.dirname(folderPath)
+            channels = ['corr', 'x', 'y']
+            if len(out):
+                for ii, channel in enumerate(out):
+                    fName = '_'.join([os.path.basename(folderPath), channels[ii], str(i + 1), filename])
+                    savePath = os.path.join(parentDir, fName)
+                    io.save_tiff(channel, savePath)
+            # exit(0)
+            else:
+                print('Result is empty.')
+
+            im0 = im1
 
     return True
